ad0a538963359fa37848570188f768304d9c8357
Eric Mellow <ericmellow@gmail.com>
2018-08-06 09:55:31 -0600

    Problem 3

--- day1.js
+++ day1.js
@@ -47,3 +47,17 @@ const combine = (array1, array2, combinedArray = []) => {
   combinedArray.push(array2.shift())
   return combine(array1, array2, combinedArray)
 }
+
+// Problem 3
+// Write a function that computes the list of the first 100 Fibonacci numbers.By definition, the first two numbers in the Fibonacci sequence are 0 and 1, and each subsequent number is the sum of the previous two.As an example, here are the first 10 Fibonnaci numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, and 34.
+
+const fibonacci = () => {
+  const sequence = [0, 1]
+  let i = 1
+  while (sequence.length < 100) {
+    let newNum = sequence[i] + sequence[i - 1]
+    sequence.push(newNum)
+    i++
+  }
+  return sequence
+}
