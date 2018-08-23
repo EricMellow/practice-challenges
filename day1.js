fd3b839420b46d7aa87417fbc46472ac5c242935
Eric Mellow <ericmellow@gmail.com>
2018-08-23 17:20:16 -0600

    Add problem 6

--- day1.js
+++ day1.js
@@ -93,3 +93,10 @@ function accum(string) {
   })
   return output.join('')
 }
+
+
+// Problem 6
+// Given an array of numbers, determine whether the sum of all of the numbers is odd or even.
+// Give your answer in string format as 'odd' or 'even'.
+// If the input array is empty consider it as: [0](array with a zero).
+
