833518d294551e7ae58e8ff86dcce1fe5ed51111
Eric Mellow <ericmellow@gmail.com>
2018-08-30 15:47:03 -0600

    Add a class

--- practice.ts
+++ practice.ts
@@ -9,3 +9,20 @@ let myVariable: any = 'This is a string';
 let num: number = 5;
 let myName: string = 'Eric';
 let myBool: boolean = true;
+
+
+class Car {
+  model: String;
+  doors: Number;
+  isElectric: Boolean;
+
+  constructor(model: String, doors: Number, isElectric: Boolean) {
+    this.model = model;
+    this.doors = doors;
+    this.isElectric = isElectric;
+  }
+
+  displayMake(): void {
+    console.log(`This car is a ${this.model}`)
+  }
+}
