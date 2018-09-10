d273a23049675668eb40c1e27f06509daa7d7e2e
Eric Mellow <ericmellow@gmail.com>
2018-09-10 16:51:45 -0600

    Add new problem

--- challenges.js
+++ challenges.js
@@ -352,3 +352,9 @@ const data = {
 function validate(password) {
   return /(?=.*[a-z])(?=.*[A-Z])(?=.*[\d])(?=.{6,})/.test(password);
 }
+
+// Problem 16
+// The main idea is to count all the occuring characters(UTF - 8) in string.If you have string like this aba then the result should be { 'a': 2, 'b': 1 }
+
+// What if the string is empty ? Then the result should be empty object literal { }
+
