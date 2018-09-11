447db1784261d55f7d6af223339b3f7fcbb55a41
Eric Mellow <ericmellow@gmail.com>
2018-09-11 16:16:29 -0600

    Add new problem

--- challenges.js
+++ challenges.js
@@ -442,4 +442,14 @@ const keyPresses = (string) => {
 
 const keyboard = [["1"], ["a", "b", "c", "2"], ["d", "e", "f", "3"], ["g", "h", "i", "4"], ["j", "k", "l", "5"], ["m", "n", "o", "6"], ["p", "q", "r", "s", "7"], ["t", "u", "v", "8"], ["w", "x", "y", "z", "9"], ["*"], [" ", "0"], ["#"]]
 
-keyPpresses(message)
+keyPresses(message)
+
+// Problem 19
+// Create the function prefill that returns an array of n elements that all have the same value v.
+
+// You have to validate input:
+// v can be anything(primitive or otherwise)
+// if v is ommited, fill the array with undefined
+// if n is 0, return an empty array
+// if n is anything other than an integer or integer - formatted string(e.g. '123') that is >= 0, throw a TypeError
+// When throwing a TypeError, the message should be n is invalid, where you replace n for the actual value passed to the function.
